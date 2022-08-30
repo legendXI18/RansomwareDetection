9453d609deb606545aa5566c94d1dd328bcc60c4
Rameez Khanzada <rameez.khanzada@city.ac.uk>
2022-08-30 16:42:28 +0100

    integrated virustotal API - checks hashes of exe - if hash doesnt exists in virustotal database, sends sample

--- VerifyExe.py
+++ VerifyExe.py
@@ -1,17 +1,51 @@
+import py as py
 import schedule
 import time
 import os
 import psutil
 import hashlib
 
+from vt import APIError
+
+import config
+import vt
+
+from twython import Twython, TwythonError
+
 from FetchProcessID import FetchProcessID
 
 p = FetchProcessID()
-runningEXEDict = {}
+virusTotalAPI = Twython(config.api_key_VT)
+client = vt.Client(virusTotalAPI.app_key)
+
+windows_services_parent_process = \
+    {
+        "wininit.exe": "smss.exe",
+        "System": "",
+        "services.exe": "wininit.exe",
+        "lsass.exe": "wininit.exe",
+        "taskhostw.exe": "svchost.exe",
+        "svchost.exe": "services.exe"
+    }
+windows_services_path = \
+    {
+        "services.exe": "C:\\Windows\\System32\\services.exe",
+        "svchost.exe": "C:\\Windows\\System32\\svchost.exe",
+        "wininit.exe": "C:\\Windows\\System32\\wininit.exe",
+        "explorer.exe": "C:\\Windows\\System32\\explorer.exe",
+    }
+def get_info_file(hash):
+
+    file = client.get_object("/files/" + str(hash))
+    return file
+
+def scan_file(path):
+     with open(path, "rb") as f:
+        analysis = client.scan_file(f, wait_for_completion=True)
+     return analysis
 
 def CheckRunningProcess():
     print(("calling setlist 1"))
-    p.getCurrentRunningProcessID()
     time.sleep(5)
     print(("calling setlist 2"))
     p.processIDlist2 = p.getCurrentRunningProcessID()
@@ -20,45 +54,112 @@ def CheckRunningProcess():
     print(combinedList)
 
     p.processIDlist1 = p.processIDlist2
-# by checking parent process - lets me find abnormal launches i.e. non windows
-    #todo:
-    # store into dictionary the name of process and pid - only one instance of certain processes should exist wininit, system, services,isass etc
-    #
-    # check parent process location
 
     for proc in combinedList:
+        running_exe_dict = {}
         try:
-
-            print("current process")
             process = psutil.Process(proc)
-            print(process.name())
-
-            if process.name() in
-
-            print("parent")
             parentProcess = psutil.Process(process.ppid())
-            print(parentProcess)
-            print(parentProcess.exe())
 
-            print("grandparent")
-            grandparentProcess = psutil.Process(parentProcess.ppid())
-            print(grandparentProcess)
-            print(grandparentProcess.exe())
+            checkProcessParent(process)
+            checkProcessParent(parentProcess)
+            checkProcessLocation(process)
+            checkProcessLocation(parentProcess)
 
-            print(checkHash(process.exe()))
+        # print("current process")
+        # process = psutil.Process(proc)
+        # process_name = process.name()
+        # print(process_name)
+        # running_exe_dict[process.pid] = process.name()
+        #
+        # print("parent")
+        # parentProcess = psutil.Process(process.ppid())
+        # print(parentProcess)
+        # print(parentProcess.exe())
+
+        # print("looking at parent process")
+        # if process_name in windows_services_parent_process.keys():
+        #     print("hit")
+        #     print(windows_services_parent_process[process_name])
+
+        # print("grandparent")
+        # grandparentProcess = psutil.Process(parentProcess.ppid())
+        # print(grandparentProcess)
+        # print(grandparentProcess.exe())
+        #
+        # if parentProcess.name() in windows_services_parent_process.keys():
+        #     print("hit")
+        #     print(windows_services_parent_process[parentProcess])
+
+        # print(checkHash(process.exe()))
 
         except Exception as e:
             print(e)
 
+    print(running_exe_dict)
+
     print("combined")
     print(combinedList)
 
-def checkHash(path):
+# checks the process parent
+def checkProcessParent(process):
 
+    parentProcess = psutil.Process(process.ppid())
+
+    if process.name() in windows_services_parent_process.keys():
+        if parentProcess.name() == windows_services_parent_process[process.name()]:
+            print(process.name() + " is verified")
+            print("dictionary print:")
+            print(windows_services_parent_process[process])
+            return True
+        else:
+            print(process.name() + " parent is not verified")
+            virusTotalVerification(process)
+    else:
+        print(process.name() + " ignore process")
+        # os.system("taskkill /im " + process.exe())
+        return False
+def virusTotalVerification(process):
+
+    try:
+        report = get_info_file(checkHash(process.exe()))
+        if report.reputation < 0:
+            print("killing process and sleeping PC")
+            #kill the Process
+            #os.system("taskkill /im " + process.exe())
+
+            #hibrinate PC
+            #os.system("shutdown /h")
+    except APIError as e:
+        if e.code == "NotFoundError":
+            try:
+                scan_file(process.exe())
+            except Exception as e:
+                print(e)
+
+
+def checkProcessLocation(process):
+    if process.name() in windows_services_parent_process.keys():
+        if process.exe() == windows_services_parent_process[process.name()]:
+            print("process location verified")
+        else:
+            print(process.exe() + " not verified")
+    else:
+        print("ignore location of " + process.name())
+
+    # by checking parent process - lets me find abnormal launches i.e. non windows
+    # todo:
+    # store into dictionary the name of process and pid - only one instance of certain processes should exist wininit, system, services,isass etc
+    #
+    # check parent process location
+
+
+def checkHash(path):
     return hashlib.md5(open(path, 'rb').read()).hexdigest()
 
+
 if __name__ == "__main__":
-   # CheckRunningProcess()
+    # CheckRunningProcess()
     schedule.every(10).seconds.do(CheckRunningProcess)
 
     while True:
